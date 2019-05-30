61338538ff9301338d345316c2b3adbd611040c0
Davis Nascimento <davis_victor@hotmail.com>
2019-05-30 17:53:44 -0300

    Menu de interação com usuário iniciado e funções que alteram a cor do texto no console adicionadas.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
-#include "Funcionario.h"
-#include "Tratador.h"
-#include "Veterinario.h"
+//#include "Funcionario.h"
+//#include "Tratador.h"
+//#include "Veterinario.h"
+#include "Interacao.h"
 #include <iostream>
 #include <map>
 
 int main(int argc, char const *argv[])
 {
-    Tratador t1(1, "Jose", "7033", 32, 2, 'H', "limpar", 1);
+    int escolha = 0;
+    menu(escolha);
+
+    //Loop para o menu sempre permanecer na tela até que o input seja 0
+    while(escolha > 0){ 
+        menu(escolha);
+    }
+
+    return 0;
+}
+
+    /*Tratador t1(1, "Jose", "7033", 32, 2, 'H', "limpar", 1);
     t1.save();
     Tratador t2(2, "Matheus", "7012", 19, 2, 'O', "cuidar", 2);
     t2.save();
     Tratador t3(3, "Maria", "70111", 22, 2, 'H', "comida", 0);
     t3.save();
     Veterinario v1(4, "Arnaldo", "7013411", 21, 2, 'H', "cura", "aa");
-    v1.setCrmv("zz");
+    v1.setCnmv("zz");
     v1.save();
     Veterinario v2(5, "Davis", "7011111", 19, 2, 'H', "cura", "bb");
     v2.save();
@@ -23,6 +35,4 @@ int main(int argc, char const *argv[])
     std::cout << "mymultimap contains:\n";
     for (auto it=funcionarios.begin(); it!=funcionarios.end(); ++it){
         std::cout << (*it).first << " => " << (*it).second->getNome() << '\n';
-    }
-    return 0;
-}
+    }*/
---
+++ src/utils/Interacao.cpp
@@ -0,0 +1,37 @@
+#include "Interacao.h"
+
+void menu(int &escolha){
+    std::cout << std::endl;
+    
+    textColor_vermelhoBold("PetFera 2019.1 | AMD - All Rights Reserved ©");
+    std::cout << std::endl;
+    textColor_vermelhoBold("============================================");
+    std::cout << std::endl;
+    
+    std::cout << "Escolha uma opção e divirta-se: " << std::endl;
+    std::cout << "    1 -> Fazer x " << std::endl;
+    std::cout << "    2 -> Fazer Y " << std::endl;
+    std::cout << "    0 -> Sair " << std::endl;
+    std::cin >> escolha;
+
+    switch (escolha)
+    {
+        case 0:
+            std::cout << "Tchau! Volte sempre" << std::endl;
+            break;
+        case 1:
+            std::cout << "Escolheu a opção 1, congrats" << std::endl;
+            break;
+        case 2:
+            std::cout << "Escolheu a opção 2, congrats" << std::endl;
+            break;
+        
+        default:
+            std::cout << "Essa opção não existe, man" << std::endl;
+            break;
+    }
+}
+
+void textColor_vermelhoBold(std::string texto){
+    std::cout << "\033[1;31m" << texto << "\033[0m";
+}
