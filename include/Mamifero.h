7308457cdaa4b630d085670d3ad6acaf699e7241
arnaldosouza <arnaldoeloi2@gmail.com>
2019-06-09 01:45:16 -0300

    Adicionados metodos construtores em Mamifero

--- include/Mamifero.h
+++ include/Mamifero.h
@@ -14,6 +14,12 @@ class Mamifero : public Animal{
         Mamifero();
         ~Mamifero(){};
 
+        Mamifero(int id, std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, std::string cor_do_pelo);
+
+        Mamifero(std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, std::string cor_do_pelo);
+
+        Mamifero(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, std::string cor_do_pelo);
+
         std::string getCorDoPelo();
         void setCorDoPelo(std::string cor_do_pelo);
 
