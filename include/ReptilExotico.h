8e8b0e46a50ec6ece0b4fd347d541f25b80771a8
arnaldosouza <arnaldoeloi2@gmail.com>
2019-06-09 01:39:11 -0300

    Adicionados metodos de printInFile para ReptilExotico

--- include/ReptilExotico.h
+++ include/ReptilExotico.h
@@ -12,13 +12,13 @@ class ReptilExotico : public Reptil, public AnimalExotico{
         
     public:
         ReptilExotico();
+        ~ReptilExotico(){};
+
         ReptilExotico(int id, std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, bool venenoso, std::string tipo_veneno, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
         
         ReptilExotico(std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, bool venenoso, std::string tipo_veneno, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
-        ~ReptilExotico(){};
         
         ReptilExotico(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, bool venenoso, std::string tipo_veneno, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
-        ~ReptilExotico(){};
 
         std::string printInFile(int id);
 };
