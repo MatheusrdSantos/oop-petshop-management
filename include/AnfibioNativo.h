8b9f37ed3359618be7a964bbdc905a0f94452132
arnaldosouza <arnaldoeloi2@gmail.com>
2019-06-10 15:42:10 -0300

    Adicionada documentaçao para AnfibioNativo.

--- include/AnfibioNativo.h
+++ include/AnfibioNativo.h
@@ -9,19 +9,87 @@ class AnfibioNativo : public Anfibio, public AnimalNativo{
     private:
 
     protected:
-        
+        /**
+         * @brief Define a forma como o texto do Anfíbio Nativo será armazenado no banco.
+         * 
+         * @param id 
+         * @return std::string 
+         */
+        std::string printInFile(int id);
     public:
+
+        /**
+         * @brief Constrói um novo objeto do tipo Anfíbio Nativo.
+         * 
+         */
         AnfibioNativo();
 
+        /**
+         * @brief Constrói um novo objeto do tipo Anfíbio Nativo.
+         * 
+         * @param id 
+         * @param classe 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param total_de_mudas 
+         * @param ultima_muda 
+         * @param uf_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AnfibioNativo(int id, std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, int total_de_mudas, Date* ultima_muda, std::string uf_origem, std::string cidade_origem, std::string autorizacao_ibama);
         
+        /**
+         * @brief Constrói um novo objeto do tipo Anfíbio Nativo.
+         * 
+         * @param classe 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param total_de_mudas 
+         * @param ultima_muda 
+         * @param uf_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AnfibioNativo(std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, int total_de_mudas, Date* ultima_muda, std::string uf_origem, std::string cidade_origem, std::string autorizacao_ibama);
         
+        /**
+         * @brief Construct a new Anfibio Nativo object
+         * 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param total_de_mudas 
+         * @param ultima_muda 
+         * @param uf_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AnfibioNativo(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, int total_de_mudas, Date* ultima_muda, std::string uf_origem, std::string cidade_origem, std::string autorizacao_ibama);
 
+        /**
+         * @brief Destroy the Anfibio Nativo object
+         * 
+         */
         ~AnfibioNativo(){};
 
-        std::string printInFile(int id);
 };
 
 #endif
