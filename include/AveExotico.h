5260fb0d1e774a6df2106afbac6b40cf388a0dcd
arnaldosouza <arnaldoeloi2@gmail.com>
2019-06-10 16:01:58 -0300

    Adicionada documentaçao para AveExotico.

--- include/AveExotico.h
+++ include/AveExotico.h
@@ -10,17 +10,88 @@ class AveExotico : public Ave, public AnimalExotico{
     private:
 
     protected:
+        /**
+         * @brief Define a forma como o texto da Ave Exótica será armazenado no banco.
+         * 
+         * @param id 
+         * @return std::string 
+         */
+        std::string printInFile(int id);
         
     public:
+        /**
+         * @brief Constrói um novo objeto do tipo Ave Exotico
+         * 
+         */
         AveExotico();
+
+        /**
+         * @brief Constrói um novo objeto do tipo Ave Exotico
+         * 
+         * @param id 
+         * @param classe 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param tamanho_do_bico_cm 
+         * @param envergadura_das_asas 
+         * @param pais_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AveExotico(int id, std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
 
+        /**
+         * @brief Constrói um novo objeto do tipo Ave Exotico
+         * 
+         * @param classe 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param tamanho_do_bico_cm 
+         * @param envergadura_das_asas 
+         * @param pais_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AveExotico(std::string classe, std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
 
+        /**
+         * @brief Constrói um novo objeto do tipo Ave Exotico
+         * 
+         * @param nome 
+         * @param nome_cientifico 
+         * @param sexo 
+         * @param tamanho 
+         * @param dieta 
+         * @param veterinario 
+         * @param tratador 
+         * @param nome_batismo 
+         * @param tamanho_do_bico_cm 
+         * @param envergadura_das_asas 
+         * @param pais_origem 
+         * @param cidade_origem 
+         * @param autorizacao_ibama 
+         */
         AveExotico(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas, std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama);
+        
+        /**
+         * @brief Destrói a Ave Exotico
+         * 
+         */
         ~AveExotico(){};
 
-        std::string printInFile(int id);
+
 };
 
 #endif
