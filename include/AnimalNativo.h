ef4e70398f05964c0b7221f65fbed625cf83f71d
arnaldosouza <arnaldoeloi2@gmail.com>
2019-06-10 15:50:40 -0300

    Adicionado documentaçao para Animal Nativo.

--- include/AnimalNativo.h
+++ include/AnimalNativo.h
@@ -9,18 +9,56 @@ class AnimalNativo : public AnimalSilvestre{
     private:
 
     protected:
-        std::string m_uf_origem;
-        std::string m_cidade_origem;
+        std::string m_uf_origem; /**< União Federativa da qual o animal foi coletado. */
+        std::string m_cidade_origem; /**< Cidade da qual o animal foi coletado. */
         
     public:
+        /**
+         * @brief Constrói um novo objeto do tipo Animal Nativo 
+         * 
+         */
         AnimalNativo();
+
+        /**
+         * @brief Constrói um novo objeto do tipo Animal Nativo 
+         * 
+         * @param uf_origem 
+         * @param cidade_origem 
+         */
         AnimalNativo(std::string uf_origem, std::string cidade_origem);
+        
+        /**
+         * @brief Destrói o Animal Nativo 
+         * 
+         */
         ~AnimalNativo(){};
 
+        /**
+         * @brief Retorna a Uf de Origem 
+         * 
+         * @return std::string 
+         */
         std::string getUfOrigem();
+
+        /**
+         * @brief Define a Uf de Origem 
+         * 
+         * @param uf_origem 
+         */
         void setUfOrigem(std::string uf_origem);
 
+        /**
+         * @brief Retorna a Cidade de Origem 
+         * 
+         * @return std::string 
+         */
         std::string getCidadeOrigem();
+
+        /**
+         * @brief Define a Cidade de Origem 
+         * 
+         * @param cidade_origem 
+         */
         void setCidadeOrigem(std::string cidade_origem);
 };
 
