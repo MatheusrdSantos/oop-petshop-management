66b5c777ef123e6d3d7737ffb1bddb7e8c3c87e3
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-20 00:11:42 -0300

    hierarquia de construtores finalizada para AnfibioExótico

--- src/classes/AnfibioExotico.cpp
+++ src/classes/AnfibioExotico.cpp
@@ -12,7 +12,7 @@ AnfibioExotico::AnfibioExotico(std::string nome, std::string nome_cientifico, ch
     m_nome_batismo = nome_batismo;
 }
 
-AnfibioExotico::AnfibioExotico(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, int total_de_mudas = 0, Date* ultima_muda = new Date(0,0,0), std::string autorizacao_ibama = NULL, std::string pais_origem = NULL, std::string cidade_origem = NULL): Anfibio(nome, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo, total_de_mudas, ultima_muda){
+AnfibioExotico::AnfibioExotico(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, int total_de_mudas = 0, Date* ultima_muda = new Date(0,0,0), std::string autorizacao_ibama = NULL, std::string pais_origem = NULL, std::string cidade_origem = NULL): Anfibio(nome, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo, total_de_mudas, ultima_muda), AnimalExotico(pais_origem, cidade_origem, autorizacao_ibama){
     m_classe = "Amphibia" ;
     m_nome_cientifico = nome_cientifico;
     m_sexo = sexo ;
---
+++ src/classes/AnimalExotico.cpp
@@ -0,0 +1,7 @@
+#include "../../include/AnimalExotico.h"
+
+AnimalExotico::AnimalExotico(std::string pais_origem, std::string cidade_origem, std::string autorizacao_ibama): AnimalSilvestre(autorizacao_ibama){
+    m_pais_origem = pais_origem;
+    m_cidade_origem = cidade_origem;
+    m_autorizacao_ibama = autorizacao_ibama;
+}
