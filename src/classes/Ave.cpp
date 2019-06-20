faf2df8f3b62f74b8b92198b400dcefa3e7f28e5
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-20 13:47:39 -0300

    Construtores de mamifero finalizados

--- src/classes/Ave.cpp
+++ src/classes/Ave.cpp
@@ -33,16 +33,7 @@ Ave::Ave(std::string classe, std::string nome, std::string nome_cientifico, char
     m_envergadura_das_asas=envergadura_das_asas;
 }
 
-Ave::Ave(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas){
-    m_classe="Aves";
-    m_nome=nome;
-    m_nome_cientifico=nome_cientifico;
-    m_sexo=sexo;
-    m_tamanho=tamanho;
-    m_dieta=dieta;
-    m_veterinario=veterinario;
-    m_tratador=tratador;
-    m_nome_batismo=nome_batismo;
+Ave::Ave(std::string nome, std::string nome_cientifico, char sexo, double tamanho, std::string dieta, Veterinario* veterinario, Tratador* tratador, std::string nome_batismo, double tamanho_do_bico_cm, double envergadura_das_asas): Animal(nome, nome_cientifico, sexo, tamanho, dieta, veterinario, tratador, nome_batismo){
     m_tamanho_do_bico_cm=tamanho_do_bico_cm;
     m_envergadura_das_asas=envergadura_das_asas;
 }
